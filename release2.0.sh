409564bdb0ca4a72be8b1242f73bbdfeba18cc5e
michele <michele.arg@hotmail.it>
2025-12-17 16:25:39 +0100

    test

--- release2.0.sh
+++ release2.0.sh
@@ -367,7 +367,7 @@ validate(){
                 --target-org "$envTarget"; then
                 echo "✅ Validazione completata con successo"
 
-                sf apex run test --tests "$test_list" --target-org "$envTarget" --result-format json --code-coverage --wait 10 >  "./Release/test-results.json"
+                sf apex run test --tests "$test_list" --target-org "$envTarget"  >  "./Release/test-results.json"
                 coverage=$(jq -r '.result.coverage.summary.orgWideCoverage' "./Release/test-results.json" | tr -d '%')
                 echo "Coverage: $coverage"
                 if (( $(echo "$coverage < 80" | bc -l) )); then
